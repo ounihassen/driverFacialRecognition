353ac27c79d8615ba9d3f6f26f8882e85e6b5a4a
Ouni Hassen <ounihassen@yahoo.fr>
2021-05-05 21:41:57 +0100

    fix

--- driver_facial_recognition.py
+++ driver_facial_recognition.py
@@ -1,8 +1,12 @@
 import os,face_recognition,cv2,re
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
 import easygui
 import numpy as np
 from datetime import datetime
 import shutil
+from PIL import Image
 
 # Chargement du vidéo
 video_capture = cv2.VideoCapture('videos/vehicule.avi')
@@ -17,40 +21,38 @@ known_face_encodings = []
 
 is_processing_fail = True
 
-# récupérer les photos des chauffeurs autorisés dans le dossier 'img/known'
-# ces photos doivent contenir un visage 
-for (dirpath, dirnames, filenames) in os.walk('img/known/'):
-    known_faces_filenames.extend(filenames)
-    break
+# fonction encodage visages autorisés
+def loadKnownDrivers():
+    # récupérer les photos des chauffeurs autorisés dans le dossier 'img/known'
+    # ces photos doivent contenir un visage 
+    for (dirpath, dirnames, filenames) in os.walk('img/known/'):
+        known_faces_filenames.extend(filenames)
+        break
+    
+    # Learning from photos
+    if len(known_faces_filenames) == 0:
+        is_processing_fail = True
+    else:
+        is_processing_fail = False
+        for filename in known_faces_filenames:
+            # si la photo ne contient pas un visage humain on va avoir une erreur
+            face = face_recognition.load_image_file('img/known/' + filename)
+            # on rempli le tableau des noms depuis les noms des photos sans l'extension
+            known_face_names.append(re.sub("[0-9]",'', filename[:-4]))
+            """
+            L'encodage est simplement une représentation de faible dimension d'une face 
+            qui peut être facilement comparée à d'autres faces que la bibliothèque 
+            reconnaîtra à l'avenir.
+            """
+            faces = face_recognition.face_encodings(face)
+            if len(faces) < 1:
+                is_processing_fail = True
+            else:
+                is_processing_fail = False
+                known_face_encodings.append(faces[0])
 
-# Learning from photos
-if len(known_faces_filenames) == 0:
-    is_processing_fail = True
-else:
-    is_processing_fail = False
-    for filename in known_faces_filenames:
-        # si la photo ne contient pas un visage humain on va avoir une erreur
-        face = face_recognition.load_image_file('img/known/' + filename)
-        # on rempli le tableau des noms depuis les noms des photos sans l'extension
-        known_face_names.append(re.sub("[0-9]",'', filename[:-4]))
-        """
-        L'encodage est simplement une représentation de faible dimension d'une face 
-        qui peut être facilement comparée à d'autres faces que la bibliothèque 
-        reconnaîtra à l'avenir.
-        """
-        faces = face_recognition.face_encodings(face)
-        if len(faces) < 1:
-            is_processing_fail = True
-        else:
-            is_processing_fail = False
-            known_face_encodings.append(faces[0])
-
-print('Learned encoding for', len(known_face_encodings), 'images.')
-
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+    print('Learned encoding for', len(known_face_encodings), 'images.')
+    return is_processing_fail
 
 # fonction qui permet de dessiner un cadre autour du visage
 def drawRectangleOnFace(face_locations, face_names, similarity_text, autorized=True ):
@@ -61,30 +63,61 @@ def drawRectangleOnFace(face_locations, face_names, similarity_text, autorized=T
         color = [0,0,255]
         name = "Unknown ("+similarity_text+")"
     for (top, right, bottom, left), name in zip(face_locations, name):
-        # Crop from x, y, w, h 
-        crop_img = frame[top: top+bottom+30, left: left+bottom] 
-        date_now = datetime.today().strftime('%Y%m%d%H%M%S')
-        img_path = 'img/unknown/unknown_'+date_now+'.jpg'
-        cv2.imwrite(img_path, crop_img)
-        # call dialog box
-        autoriseUnknownDriver(img_path)
+        if not autorized:
+           captureFace(frame, top, right, bottom, left)
+        
         # draw rectangle
         cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name+"("+similarity_text+")", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1) 
+
+# fonction accept the new driver to drive the car and save image
+def captureFace(frame, top, right, bottom, left):
+    crop_img = frame[top: top+bottom+30, left: left+bottom] 
+    date_now = datetime.today().strftime('%Y%m%d%H%M%S')
+    img_path = 'img/unknown/unknown_'+date_now+'.jpg'
+    if cv2.imwrite(img_path, crop_img):
+        image = Image.open(img_path)
+        image.show()
+        autoriseUnknownDriver(img_path)
         
 # fonction accept the new driver to drive the car and save image
 def autoriseUnknownDriver(img_path):
-    #cv2.imread(img_path)
-    answear_yes = easygui.enterbox('Do you want to authorize this person to drive your car?', 'Alert', "Enter user name..")
-    if answear_yes:
-        shutil.move(img_path, 'img/known/'+answear_yes+'.jpg')         
-    else:
-        easygui.msgbox('The driver is not allowed to drive this car', 'Warning')
-       
-    
+    top = Tk()
+    #top.geometry("300x200")
+    response = messagebox.askquestion("autorise unknown driver", "Do you want to authorize this person to drive your car ?", icon='warning')
+    print(response)
+    if response == 'yes':
+        canvas1 = tk.Canvas(top, width = 400, height = 300,  relief = 'raised')
+        canvas1.pack()
+        
+        label1 = tk.Label(top, text='Authorization of a new driver')
+        label1.config(font=('helvetica', 14))
+        canvas1.create_window(200, 25, window=label1)
+        
+        label2 = tk.Label(top, text='please enter the first name of the new driver:')
+        label2.config(font=('helvetica', 10))
+        canvas1.create_window(200, 100, window=label2)
+        
+        entry1 = tk.Entry(top) 
+        canvas1.create_window(200, 140, window=entry1)
+        
+        def getName():
+            name = entry1.get()
+            shutil.move(img_path, 'img/known/'+name+'.jpg')
+            loadKnownDrivers()
+            top.destroy()
+            
+        button1 = tk.Button(text='Valider', command=getName, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+        canvas1.create_window(200, 180, window=button1)
+        top.mainloop()
+
+
+face_locations = []
+face_encodings = []
+face_names = []
 while True:
-    if is_processing_fail:
+    if loadKnownDrivers():
         break
         
     ret, frame = video_capture.read()
